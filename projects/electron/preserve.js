f8512c937b4096663e9f2a856892b9ca01ac9575
mint-dev <>
2019-03-20 21:37:21 +0300

    preserve

--- projects/electron/preserve.js
+++ projects/electron/preserve.js
@@ -4,8 +4,8 @@ const path = 'node_modules/@materia-ui/ngx-monaco-editor/fesm2015/materia-ui-ngx
 fs.readFile(path, 'utf8', (err, data) => {
   fs.writeFile(
     path,
-    data.replace(/'assets\/monaco-editor\/min\/vs'/g, '__dirname + \'/assets/monaco-editor/min/vs\''),
+    data.replace(/__dirname \+ '\/assets\/monaco-editor\/min\/vs'/g, "'assets/monaco-editor/min/vs'"),
     'utf8',
-    err => err ? console.log('[PREBUILD] Error', err) : console.log('[PREBUILD] Success')
+    err => err ? console.log('[PRESERVE] Error', err) : console.log('[PRESERVE] Success')
   );
 });
